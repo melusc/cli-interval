0045698ef130fbde7cf4162ceec0c9b1303cb44a
melusc <61409445+melusc@users.noreply.github.com>
2021-05-12 16:59:56 +0200

    Better error-handling Remove is-number

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import {exec} from 'node:child_process';
 
 import ms from 'ms';
 import meow from 'meow';
-import isNumber from 'is-number';
 
 const cli = meow(
 	`
@@ -42,13 +41,23 @@ if (cli.version) {
 	cli.showVersion();
 }
 
-const toMilliseconds = value => (isNumber(value) ? Number(value) : ms(value));
-
 const command = cli.input[0];
-const interval = toMilliseconds(cli.flags.interval);
+const interval = ms(cli.flags.interval);
+
+if (interval === undefined) {
+	throw new Error('ms could not interpret interval-string.');
+}
+
+if (!command) {
+	cli.showHelp();
+}
 
 const intervalFunction = () => {
-	const child = exec(command, () => {
+	const child = exec(command, error => {
+		if (error) {
+			throw error;
+		}
+
 		setTimeout(intervalFunction, interval);
 	});
 	child.stdout.pipe(process.stdout);
